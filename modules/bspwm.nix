2deb85e5b527e5bb19b0556402bfc8ff15ab732e
Seraphina <xploitrisgod@gmail.com>
2021-07-02 12:16:48 +0200

    make bspwm more usable as a wm

--- modules/bspwm.nix
+++ modules/bspwm.nix
@@ -1,31 +1,37 @@
 {pkgs, ...}: {
-  xsession.windowManager.bspwm = {
-    enable = true;
-    
-    monitors = {
-      HDMI-1 = ["一" "二" "三" "四" "五"];
-    };
-
-    rules = {
-      "firefox" = {
-        desktop = "^3";
+  xsession = {
+    windowManager.bspwm = {
+      enable = true;
+      
+      monitors = {
+        HDMI-1 = ["一" "二" "三" "四" "五"];
       };
-      "Discord" = {
-        desktop = "^2";
+
+      rules = {
+        "firefox" = {
+          desktop = "^3";
+        };
+        "ripcord" = {
+          desktop = "^2";
+        };
+      };
+
+      startupPrograms = [
+
+        "systemctl --user import-environment DISPLAY"
+        "xset r rate 200 60"
+        "sxhkd "
+        "fcitx"
+        "alacritty -e tmux "
+        "firefox "
+        "ripcord"
+      ];
+
+      settings = {
+        border_width = 0;
+        window_gap = 0;
+        ignore_ewmh_focus = true;
       };
     };
-
-    settings = {
-      border_width = 0;
-      window_gap = 0;
-    };
-
-    startupPrograms = [
-      "systemctl --user import-environment DISPLAY"
-      "sxhkd"
-      "st"
-      "firefox"
-      "Discord"
-    ];
   };
 }
--- modules/sxhkd.nix
+++ modules/sxhkd.nix
@@ -3,12 +3,18 @@
     enable = true;
     keybindings = {
       "super + x" = "dmenu_run";
-      "super + Return" = "st";
+      "super + Return" = "alacritty";
       "super + shift + r" = "bspc wm -r";
+      "super + shift + c" = "bspc node -k";
       "super + {_, shift + } {1-5,0}" = "bspc {desktop -f, node -d} '^{1-5,10}'";
       "super + Escape" = "	pkill -USR1 -x sxhkd";
       "super + e" = "kaomoji-dmenu";
-      "super + s" = "screenshot";
+      "super + shift + s" = "screenshot";
+      "super + s" = "screenshot-clipboard";
+      "super + w" = "notify-info weather";
+      "super + t" = "notify-info time";
+      "super + f" = "bspc node -t floating";
+      "super + shift + f" = "bspc node -t fullscreen";
     };
   };
 }
