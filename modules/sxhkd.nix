a9d7584e88c441c12eb11db7023162b2295c525f
Seraphina <xploitrisgod@gmail.com>
2021-07-19 17:58:14 +0200

    sxhkd: set mod key to alt for all shortcuts not connected to bspwm directly

--- modules/sxhkd.nix
+++ modules/sxhkd.nix
@@ -2,19 +2,20 @@
   services.sxhkd = {
     enable = true;
     keybindings = {
-      "super + x" = "dmenu_run";
-      "super + Return" = "alacritty";
+      "alt + x" = "dmenu_run";
+      "alt + Return" = "alacritty";
       "super + shift + r" = "bspc wm -r";
       "super + shift + c" = "bspc node -k";
       "super + {_, shift + } {1-5,0}" = "bspc {desktop -f, node -d} '^{1-5,10}'";
       "super + Escape" = "	pkill -USR1 -x sxhkd";
-      "super + e" = "kaomoji-dmenu";
-      "super + shift + s" = "screenshot";
-      "super + s" = "screenshot-clipboard";
-      "super + w" = "notify-info weather";
-      "super + t" = "notify-info time";
+      "alt + e" = "kaomoji-dmenu";
+      "alt + shift + s" = "screenshot";
+      "alt + s" = "screenshot-clipboard";
+      "alt + w" = "notify-info weather";
+      "alt + t" = "notify-info time";
       "super + f" = "bspc node -t floating";
       "super + shift + f" = "bspc node -t fullscreen";
+      "super + t" = "bspc node -t tiled";
     };
   };
 }
